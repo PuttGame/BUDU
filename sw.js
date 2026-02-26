a8df512f648cc14ad33761a84960dac6b0590d45
PuttGame <haputt22@gmail.com>
2026-02-26 15:59:32 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,24 @@
-const CACHE_NAME = 'budy-pwa-v4';
+const CACHE_NAME = 'budu-v1';
+const BASE_PATH = '/BUDU/';
+const urlsToCache = [
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'manifest.json',
+  BASE_PATH + 'assets/logo.png',
+  BASE_PATH + 'assets/icon-192.png',
+  BASE_PATH + 'assets/icon-512.png'
+];
 
 self.addEventListener('install', event => {
-  console.log('[SW] Install started');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('[SW] Caching files');
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/style.css',
-        '/script.js',
-        '/logo.png',
-        '/splashscreen.png',
-        '/loadsplash.png',
-        '/icons/icon-192.png',
-        '/icons/icon-512.png'
-      ]).then(() => {
-        console.log('[SW] Cache complete');
-      }).catch(err => console.error('[SW] Cache error:', err));
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('[SW] Activate');
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
