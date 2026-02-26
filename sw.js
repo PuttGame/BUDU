59432be7d76930e539a6640dacc3775391d732b0
PuttGame <haputt22@gmail.com>
2026-02-26 16:54:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,12 +7,25 @@ const urlsToCache = [
   BASE_PATH + 'assets/logo.png',
   BASE_PATH + 'assets/icon-192.png',
   BASE_PATH + 'assets/icon-512.png'
+  // Tambah aset game lain di sini nanti, misal: BASE_PATH + 'game.js', dll
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())  // Langsung aktifkan SW baru
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())  // Ambil kontrol client langsung
   );
 });
 
